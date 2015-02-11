0d18378cf23ce1a134d47d6f20248f0dd73c32ac
joey.he <joey.he8x@qq.com>
2015-02-11 18:13:33 +0800

    update

--- src/main/scala/bz/ClubSupervisor.scala
+++ src/main/scala/bz/ClubSupervisor.scala
@@ -1,10 +1,12 @@
 package bz
 
-import akka.actor.{ActorLogging, Actor}
+import akka.actor.{Actor, ActorLogging}
+import akka.pattern.pipe
 import bz.dao.ClubDao
 import bz.model.{Club, User}
-import wechat.model.{WechatTextResponse, WechatTextMsg}
-import scala.util.{Success,Failure}
+import reactivemongo.core.commands.LastError
+import wechat.model.{WechatTextMsg, WechatTextResponse}
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
@@ -16,13 +18,22 @@ class ClubSupervisor extends Actor with ActorLogging{
       implicit val m = msg
       val boundSender = sender
       log.info(s"receive ClubCreateEvent: $cb,$user")
-      ClubDao.insert(cb).onComplete {
-        case Failure(e) =>
-          log.error("Fail to insert Club",e.getMessage)
-          boundSender ! new WechatTextResponse("创建Club失败")
-        case Success(lastError) =>
-          boundSender ! new WechatTextResponse(s"成功创建Club:$cb")
+      val promise = ClubDao.insert(cb).map[WechatTextResponse]{
+        e:LastError =>
+          if (e.inError){
+            log.error("Fail to insert Club",e.getMessage)
+            new WechatTextResponse(e.errMsg.get)
+          }else
+            new WechatTextResponse(s"成功创建Club:$cb")
       }
+      promise pipeTo sender
+//      promise.onComplete {
+//        case Failure(e) =>
+//          log.error("Fail to insert Club",e.getMessage)
+//          boundSender ! new WechatTextResponse("创建Club失败")
+//        case Success(lastError) =>
+//          boundSender ! new WechatTextResponse(s"成功创建Club:$cb")
+//      }
   }
 }
 
